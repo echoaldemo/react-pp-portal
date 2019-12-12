dcfc160314274c7ebe6cb9860a3742298c1e285c
Jech <jericho.aldemo@boom.camp>
2019-12-12 09:48:45 +0800

    deleted dist

--- dist/App.js
+++
@@ -1,7 +0,0 @@
-import './App.css';
-import TestMainView from './views/test-main-view/TestMainView';
-function App() {
-    // Here you will use context api/router etc.
-}
-export default TestMainView;
-//# sourceMappingURL=App.js.map
--- dist/contexts/TestContext.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=TestContext.js.map
