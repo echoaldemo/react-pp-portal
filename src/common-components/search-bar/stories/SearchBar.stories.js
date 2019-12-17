3d19d3c8b44ef37f737f54ecc142b690b6313d6b
Jech <jericho.aldemo@boom.camp>
2019-12-17 14:00:16 +0800

    finished side nav

--- src/common-components/search-bar/stories/SearchBar.stories.js
+++ src/common-components/search-bar/stories/SearchBar.stories.js
@@ -1,7 +1,7 @@
 import React, { createElement } from "react";
 // import notes from "./notes.md";
 import { storiesOf } from "@storybook/react";
-import SearchBar from "../index";
+import { SearchBar } from "..";
 const stories = storiesOf("SeachBar", module);
 stories.add("SeachBar", () =>
   createElement(() => {
