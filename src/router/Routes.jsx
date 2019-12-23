3a967191413e79394d22f5cc214607bf33f8c9fb
Jules Ballaran <jules.ballaran@boom.camp>
2019-12-23 16:45:38 +0800

    add gateway component

--- src/router/Routes.jsx
+++ src/router/Routes.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React from 'react'
+import { BrowserRouter, Switch } from 'react-router-dom'
 
-import { PublicRoute, GatewayRoute, PrivateRoute } from './';
-import Gateway from 'components/gateway';
-import Sample from 'components/sample-component';
+import { PublicRoute, GatewayRoute, PrivateRoute } from './'
+import Gateway from 'components/gateway'
+import Sample from 'components/sample-component'
 export default function Routes() {
-	return (
-		<BrowserRouter>
-			<Switch>
-				<PublicRoute exact path="/" component={Sample} />
-				<GatewayRoute path="/gateway" component={Gateway} />
-				<PrivateRoute path="/manage/sample" component={() => <div>xx</div>} />
-			</Switch>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Switch>
+        <PublicRoute exact path="/" component={Sample} />
+        <GatewayRoute path="/gateway" component={Gateway} />
+        <PrivateRoute path="/manage/sample" component={() => <div>xx</div>} />
+      </Switch>
+    </BrowserRouter>
+  )
 }
