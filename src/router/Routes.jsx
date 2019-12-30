54e12f75402b853e8e9c541bf08d7044d918f0c9
Sidney Bercasio <sidney.bercasio@boom.camp>
2019-12-30 05:58:21 +0000

    Bc develop signin view

--- src/router/Routes.jsx
+++ src/router/Routes.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import { BrowserRouter, Switch } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Switch } from "react-router-dom";
 
-import { PublicRoute, GatewayRoute, PrivateRoute } from './'
-import Gateway from 'components/gateway'
-import Sample from 'components/sample-component'
+import { PublicRoute, GatewayRoute, PrivateRoute } from "./";
+import Gateway from "components/gateway";
+import Signin from "auth/component";
+import Sample from "components/sample-component";
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <PublicRoute exact path="/" component={Sample} />
+        <PublicRoute exact path="/" component={Signin} />
         <GatewayRoute path="/gateway" component={Gateway} />
         <PrivateRoute path="/manage/sample" component={() => <div>xx</div>} />
       </Switch>
     </BrowserRouter>
-  )
+  );
 }
