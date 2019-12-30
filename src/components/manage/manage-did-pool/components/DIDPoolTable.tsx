67bf5f130a951893d4ad748c9fe7c41ea9627a33
Jules Ballaran <jules.ballaran@boom.camp>
2019-12-30 15:57:17 +0800

    fix table height

--- src/components/manage/manage-did-pool/components/DIDPoolTable.tsx
+++ src/components/manage/manage-did-pool/components/DIDPoolTable.tsx
@@ -16,62 +16,55 @@ const DIDPoolTable: React.FC<DidPoolTableProps> = ({ state, history }) => {
   const [copy, setCopy] = useState(false)
 
   return (
-    <>
-      <AsyncTable
-        headers={headers}
-        tableData={state.didPools}
-        render={(pools: any, { row, cell, uuid, icon }: any) =>
-          pools.map((pool: any) => (
-            <TableRow className={row} key={pool.uuid}>
-              <UnderlineCell
-                className={cell}
-                onClick={() =>
+    <AsyncTable
+      headers={headers}
+      tableData={state.didPools}
+      render={(pools: any, { row, cell, uuid, icon }: any) =>
+        pools.map((pool: any) => (
+          <TableRow className={row} key={pool.uuid}>
+            <UnderlineCell
+              className={cell}
+              onClick={() => history.push(`/manage/did-pool/edit/${pool.uuid}`)}
+            >
+              {pool.name}
+            </UnderlineCell>
+            <TableCell className={uuid}>
+              <p>{pool.uuid}</p>
+              <CopyToClipboard
+                text={pool.uuid}
+                onCopy={() => setCopy(true)}
+                onPointerLeave={() => setCopy(false)}
+              >
+                {copy ? (
+                  <LightTooltip title="UUID Copied!" placement="top">
+                    <Icon className={icon} rotate={360} />
+                  </LightTooltip>
+                ) : (
+                  <LightTooltip title="Copy UUID" placement="top">
+                    <Icon className={icon} rotate={360} />
+                  </LightTooltip>
+                )}
+              </CopyToClipboard>
+            </TableCell>
+            <ActiveCell className={cell}>{pool.active}</ActiveCell>
+            <ActiveCell className={cell}>
+              {pool.allow_inbound ? 'yes' : 'no'}
+            </ActiveCell>
+            <TableCell className={cell} style={{ textDecoration: 'underline' }}>
+              {pool.did_count}
+            </TableCell>
+            <TableCell className={cell} align="right">
+              <EditButton
+                text="Edit"
+                onClickFunc={() =>
                   history.push(`/manage/did-pool/edit/${pool.uuid}`)
                 }
-              >
-                {pool.name}
-              </UnderlineCell>
-              <TableCell className={uuid}>
-                <p>{pool.uuid}</p>
-                <CopyToClipboard
-                  text={pool.uuid}
-                  onCopy={() => setCopy(true)}
-                  onPointerLeave={() => setCopy(false)}
-                >
-                  {copy ? (
-                    <LightTooltip title="UUID Copied!" placement="top">
-                      <Icon className={icon} rotate={360} />
-                    </LightTooltip>
-                  ) : (
-                    <LightTooltip title="Copy UUID" placement="top">
-                      <Icon className={icon} rotate={360} />
-                    </LightTooltip>
-                  )}
-                </CopyToClipboard>
-              </TableCell>
-              <ActiveCell className={cell}>{pool.active}</ActiveCell>
-              <ActiveCell className={cell}>
-                {pool.allow_inbound ? 'yes' : 'no'}
-              </ActiveCell>
-              <TableCell
-                className={cell}
-                style={{ textDecoration: 'underline' }}
-              >
-                {pool.did_count}
-              </TableCell>
-              <TableCell className={cell} align="right">
-                <EditButton
-                  text="Edit"
-                  onClickFunc={() =>
-                    history.push(`/manage/did-pool/edit/${pool.uuid}`)
-                  }
-                />
-              </TableCell>
-            </TableRow>
-          ))
-        }
-      />
-    </>
+              />
+            </TableCell>
+          </TableRow>
+        ))
+      }
+    />
   )
 }
 
