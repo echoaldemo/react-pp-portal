7749835b2eb619c219c10d416134c52a10963fa4
Jude Agagad <judeagagad@boom.camp>
2020-01-03 00:34:02 +0800

    Minor fix: routes

--- src/router/Routes.jsx
+++ src/router/Routes.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { BrowserRouter, Switch } from "react-router-dom";
 
-import { PublicRoute, GatewayRoute, PrivateRoute } from "./";
 import Gateway from "components/gateway";
-import Sample from "components/sample-component";
 import { Realms, Campaigns } from "components";
 import { PublicRoute, GatewayRoute, PrivateRoute } from "./";
-import Gateway from "components/gateway";
 import DidPool from "components/manage/manage-did-pool";
 import Signin from "auth/component";
-import Sample from "components/sample-component";
 export default function Routes() {
   return (
     <BrowserRouter>
