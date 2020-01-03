db0460bdebd50f4a2ad16401836304aa60880924
Jomar Bandol <jomar.bandol@boom.camp>
2020-01-03 14:03:35 +0800

    WIP: manage dids

--- src/router/Routes.jsx
+++ src/router/Routes.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import { BrowserRouter, Switch } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Switch } from "react-router-dom";
 
-import Gateway from 'components/gateway'
-import { Realms, Campaigns } from 'components'
-import { PublicRoute, GatewayRoute, PrivateRoute } from './'
-import DidPool from 'components/manage/manage-did-pool'
-import Signin from 'auth/component'
+import Gateway from "components/gateway";
+import { Realms, Campaigns } from "components";
+import { PublicRoute, GatewayRoute, PrivateRoute } from "./";
+import DidPool from "components/manage/manage-did-pool";
+import Signin from "auth/component";
+import ManageDIDs from "components/manage/did/did-landing";
 export default function Routes() {
   return (
     <BrowserRouter>
@@ -15,7 +16,8 @@ export default function Routes() {
         <PrivateRoute path="/manage/realms" component={Realms} />
         <PrivateRoute path="/manage/campaigns" component={Campaigns} />
         <PrivateRoute path="/manage/did-pool" component={DidPool} />
+        <PrivateRoute path="/manage/dids" component={ManageDIDs} />
       </Switch>
     </BrowserRouter>
-  )
+  );
 }
