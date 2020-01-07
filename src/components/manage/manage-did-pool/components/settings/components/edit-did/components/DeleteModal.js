dfe0872a8130da68e2c8bfefad090678e52c27e8
Jules Ballaran <jules.ballaran@boom.camp>
2020-01-07 17:31:36 +0800

    add did table

---
+++ src/components/manage/manage-did-pool/components/settings/components/edit-did/components/DeleteModal.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { DeleteModal as DelModal } from 'common-components'
+
+export default function DeleteModal(props) {
+  return (
+    <div>
+      <DelModal
+        open={props.open}
+        header="Delete DID"
+        msg="DID"
+        name={props.did ? props.did.number : ''}
+        closeFn={() => props.closeFn()}
+        delFn={() => props.delFn()}
+      />
+    </div>
+  )
+}
