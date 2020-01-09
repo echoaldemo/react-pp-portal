fd09a2a57d671051089eb6635638aa5407f8ba01
Jules Ballaran <jules.ballaran@boom.camp>
2020-01-09 10:23:48 +0800

    add typescriot

---
+++ src/components/manage/manage-did-pool/components/settings/DidPoolsSettings.tsx
@@ -0,0 +1,408 @@
+import React, { Component } from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import { get } from 'utils/api'
+import { Paper, Dialog } from '@material-ui/core'
+import DidPoolsUpdate from './DidPoolsUpdate'
+import { menus } from '../../utils/const-var'
+import Create from './components/CreateDid'
+import Validation from './components/validate-did/ValidateDid'
+import {
+	ShoppingCartOutlined as Shopping,
+	PlaylistAddCheckOutlined as Playlist,
+	Add
+} from '@material-ui/icons'
+
+/*COMPONENTS*/
+import {
+	TableLoader,
+	LoadingModal,
+	SuccessModal,
+	SaveButton,
+	HeaderLink
+} from 'common-components'
+import DidTable from './components/DidTable'
+import styles from './DidPoolsSettings.styles'
+import BuyDid from './buy-did-number/BuyDid'
+import styled from 'styled-components'
+import DidPurchase from './did-purchase/DidPurchase'
+import Successful from './did-purchase/successful'
+import { style } from '@material-ui/system'
+
+const CustomDialog = styled(Dialog)`
+  .MuiDialog-paperScrollPaper {
+    display: flex;
+    max-height: none;
+    flex-direction: column;
+  }
+`
+
+const dids = [
+	{
+		number: '(406) 262-8717',
+		purchased: true
+	},
+	{
+		number: '(406) 262-8718',
+		purchased: true
+	},
+	{
+		number: '(406) 262-8719',
+		purchased: true
+	},
+	{
+		number: '(406) 262-8720',
+		purchased: false
+	},
+	{
+		number: '(406) 262-8721',
+		purchased: true
+	},
+	{
+		number: '(406) 262-8722',
+		purchased: true
+	},
+	{
+		number: '(406) 262-8723',
+		purchased: false
+	},
+	{
+		number: '(406) 262-8724',
+		purchased: false
+	}
+]
+
+interface Props {
+	match: any
+	history: any
+}
+
+interface State {
+	modalA: boolean
+	modalB: boolean
+	modalC: boolean
+	loadingDid: boolean
+	didData: any
+	company: any
+	campaign: any
+	companySlug: string
+	campaignSlug: string
+	poolId: string
+	openNew: boolean
+	openValidate: boolean
+	openBuy: boolean
+	loading: boolean
+	showResults: boolean
+	successOder: boolean
+	successError: boolean
+	openEdit: boolean
+	editData: any
+	didList: any
+}
+
+const initialBuy = {
+	loading: false,
+	showResults: false,
+	successOder: false,
+	successError: false
+}
+
+class DidPoolsSettings extends Component<Props, State> {
+	constructor(props: any) {
+		super(props)
+
+		this.state = {
+			modalA: false,
+			modalB: false,
+			modalC: false,
+			loadingDid: true,
+			didData: [],
+			company: [],
+			campaign: [],
+			companySlug: '',
+			campaignSlug: '',
+			poolId: '',
+			openNew: false,
+			openValidate: false,
+			openBuy: false,
+			loading: false,
+			showResults: false,
+			successOder: false,
+			successError: false,
+			openEdit: false,
+			editData: null,
+			didList: []
+		}
+
+	}
+	async componentDidMount() {
+		this.fetchDID()
+		this.setState({
+			poolId: this.props.match.params.uuid
+		})
+	}
+
+	handleOpenEdit = (data: any) => {
+		this.setState({
+			openEdit: true,
+			editData: data
+		})
+	}
+
+	fetchDID = () => {
+		this.setState({ loadingDid: false })
+		get(
+			`/did/company/all/campaign/all/pool/${this.props.match.params.uuid}/`
+		).then((res: any) => {
+			this.setState({
+				didData: res.data,
+				loadingDid: false
+			})
+			Promise.all([
+				get(`/identity/company/${res.data.company}`),
+				get(`/identity/campaign/${res.data.campaign}`)
+			]).then(data => {
+				if (data.length > 1) {
+					this.setState({
+						companySlug: data[0].data.slug,
+						campaignSlug: data[1].data.slug
+					})
+					this.fetchDIDlist(
+						data[0].data.slug,
+						data[1].data.slug,
+						this.props.match.params.uuid
+					)
+				}
+			})
+		})
+	}
+
+	renderCreate = () => {
+		return (
+			<Create
+				poolId={this.state.poolId}
+				companySlug={this.state.companySlug}
+				campaignSlug={this.state.campaignSlug}
+				open={this.state.openNew}
+				onClose={() => this.setState({ openNew: false })}
+			/>
+		)
+	}
+
+	renderValidate = () => {
+		return (
+			<>
+				<Validation
+					poolId={this.state.poolId}
+					companySlug={this.state.companySlug}
+					campaignSlug={this.state.campaignSlug}
+					open={this.state.openValidate}
+					onClose={() => this.setState({ openValidate: false })}
+				/>
+			</>
+		)
+	}
+	toggleBuy = () => {
+		this.setState({
+			openBuy: !this.state.openBuy
+		})
+		setTimeout(() => {
+			this.setState(initialBuy)
+		}, 500)
+	}
+
+	buyAgain = () => {
+		this.setState(initialBuy)
+	}
+
+	placeOrder = (length: number) => {
+		if (length === 1) {
+			this.setState({
+				loading: true,
+				showResults: false
+			})
+			setTimeout(() => {
+				this.setState({
+					loading: false,
+					successOder: true
+				})
+			}, 500)
+		} else {
+			this.setState({
+				loading: true,
+				showResults: false
+			})
+			setTimeout(() => {
+				this.setState({
+					loading: false,
+					successError: true
+				})
+			}, 500)
+		}
+	}
+
+	fetchDIDlist = (company: string, campaign: string, uuid: string) => {
+		get(`/did/company/${company}/campaign/${campaign}/pool/${uuid}/did/`).then(
+			(result: any) => {
+				this.setState({
+					didList: result.data
+				})
+			}
+		)
+	}
+
+	fetchDIDlistNoParams = () => {
+		get(
+			`/did/company/${this.state.companySlug}/campaign/${this.state.campaignSlug}/pool/${this.props.match.params.uuid}/did/`
+		).then((result: any) => {
+			this.setState({
+				didList: result.data
+			})
+		})
+	}
+
+	render() {
+		return (
+			<>
+				{this.renderCreate()}
+				{this.renderValidate()}
+				<div style={{ marginBottom: '50px' }}>
+					<DidPoolsUpdate
+						uuid={this.props.match.params.uuid}
+						history={this.props.history}
+					/>
+				</div>
+				<div className="header-container">
+					<HeaderLink menu={menus} title="DID's" />
+					<div style={{ display: 'flex' }}>
+						<SaveButton
+							style={{
+								marginRight: 24,
+								backgroundColor: 'transparent',
+								width: '200px'
+							}}
+							onClick={() => {
+								this.setState({
+									openNew: true
+								})
+							}}
+						>
+							<div
+								style={{
+									color: '#1194f6',
+									display: 'flex',
+									alignItems: 'center'
+								}}
+							>
+								<Add /> Add existing DID
+              </div>
+						</SaveButton>
+						<SaveButton
+							style={{
+								marginRight: 24,
+								backgroundColor: '#7c8a97',
+								width: '160px'
+							}}
+							onClick={() => {
+								this.setState({
+									openValidate: true
+								})
+							}}
+						>
+							<Playlist style={{ width: '25px', height: '20px' }} />
+							Validate ID's
+            </SaveButton>
+						<SaveButton
+							style={{ width: '160px' }}
+							onClick={() => {
+								this.setState({ openBuy: true })
+							}}
+						>
+							<Shopping
+								style={{ width: '20px', height: '20px', marginRight: '2px' }}
+							/>{' '}
+							Buy DID
+            </SaveButton>
+					</div>
+				</div>
+				<Paper style={{ height: 'auto' }}>
+					{this.state.loadingDid ? (
+						<TableLoader />
+					) : (
+							<>
+								<DidTable
+									openEdit={this.state.openEdit}
+									handleOpenEdit={this.handleOpenEdit}
+									handleCloseEdit={() => this.setState({ openEdit: false })}
+									editData={this.state.editData}
+									history={this.props.history}
+									didList={this.state.didList}
+									fetchDIDList={this.fetchDIDlistNoParams}
+									campaignSlug={this.state.campaignSlug}
+									companySlug={this.state.companySlug}
+								/>
+							</>
+						)}
+				</Paper>
+
+				{this.state.loading ? (
+					<LoadingModal
+						open={this.state.openBuy}
+						text={`One moment. We're loading the results...`}
+						cancelFn={this.toggleBuy}
+					/>
+				) : this.state.successOder ? (
+					<SuccessModal
+						open={this.state.openBuy}
+						text={'Your order has been placed succesfully'}
+						qty={200}
+						subtitle={
+							'We are processing your order. This could take up to one hour. You will receive a confirmation email when the process is done.'
+						}
+						btnText={'BUY ANOTHER'}
+						closeFn={this.toggleBuy}
+						btnFn={this.buyAgain}
+					/>
+				) : this.state.successError ? (
+					<Successful
+						open={this.state.openBuy}
+						text={'Your order has been placed succesfully'}
+						dids={dids}
+						subtitle={
+							'Some dids were not bought because they are already taken.'
+						}
+						btnText={'BUY ANOTHER'}
+						closeFn={this.toggleBuy}
+						btnFn={this.buyAgain}
+					/>
+				) : this.state.showResults ? (
+					<DidPurchase
+						header="Buy DID number"
+						closeFn={this.toggleBuy}
+						placeOrder={this.placeOrder}
+					/>
+				) : (
+									<Dialog open={this.state.openBuy}
+										maxWidth={this.state.showResults ? "lg" : false}
+										fullWidth={this.state.showResults ? true : false}
+
+									>
+										<BuyDid
+											header="Buy DID number"
+											closeFn={this.toggleBuy}
+											searchFn={() => {
+												this.setState({
+													loading: true
+												})
+												setTimeout(() => {
+													this.setState({ showResults: true, loading: false })
+												}, 1000)
+											}}
+										/>
+									</Dialog>
+								)}
+			</>
+		)
+	}
+}
+
+export default withStyles(styles)(DidPoolsSettings)
--- src/components/manage/manage-did-pool/components/settings/did-purchase/successful.js
+++ src/components/manage/manage-did-pool/components/settings/did-purchase/successful.js
@@ -1,12 +1,13 @@
-import React from "react";
-import styled from "styled-components";
-import { Close, Check, Error, CheckCircle } from "@material-ui/icons";
+import React from 'react'
+import styled from 'styled-components'
+import { Close, Check, Error, CheckCircle } from '@material-ui/icons'
+import { Dialog } from '@material-ui/core'
 
 const Center = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`;
+`
 const Card = styled.div`
   display: flex;
   flex-direction: column;
@@ -16,7 +17,7 @@ const Card = styled.div`
   height: auto;
   box-shadow: 0 0 7px 1px rgba(0, 0, 0, 0.12);
   background-color: #ffffff;
-`;
+`
 const CheckIcon = styled(Check)`
   background: #b6d36b;
   color: white;
@@ -27,14 +28,14 @@ const CheckIcon = styled(Check)`
     width: 18px !important;
     height: 13.8px !important;
   }
-`;
+`
 const CloseIcon = styled(Close)`
   color: #444851;
-`;
+`
 const CloseIconCont = styled.div`
   margin: 20px 18.5px 10px auto;
   cursor: pointer;
-`;
+`
 const CloseBtn = styled.button`
   width: 165px;
   height: 40px;
@@ -44,7 +45,7 @@ const CloseBtn = styled.button`
   cursor: pointer;
   border: none;
   outline: none;
-`;
+`
 const BtnFn = styled.button`
   width: 165px;
   height: 40px;
@@ -53,7 +54,7 @@ const BtnFn = styled.button`
   cursor: pointer;
   border: none;
   outline: none;
-`;
+`
 const BtnText = styled.span`
   width: 54px;
   height: 16px;
@@ -61,7 +62,7 @@ const BtnText = styled.span`
   font-weight: 600;
   text-align: center;
   color: #ffffff;
-`;
+`
 const CloseText = styled.span`
   width: 54px;
   height: 16px;
@@ -70,13 +71,13 @@ const CloseText = styled.span`
   text-align: center;
   color: #444851;
   text-transform: uppercase;
-`;
+`
 const BtnCont = styled.div`
   display: flex;
   justify-content: space-evenly;
   width: 100%;
   margin: 27px 0 34px 0;
-`;
+`
 const Text = styled.div`
   margin-top: 20px;
   width: 340px;
@@ -85,14 +86,14 @@ const Text = styled.div`
   font-weight: 600;
   text-align: center;
   color: #7c8a97;
-  font-family: "Roboto", "Helvetica", "Arial", sans-serif;
-`;
+  font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+`
 const P = styled.p`
   color: #7c8a97;
   margin: 10px 0 40px 0;
   max-width: 75%;
-  font-family: "Roboto", "Helvetica", "Arial", sans-serif;
-`;
+  font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+`
 
 const SubDiv = styled.div`
   display: grid;
@@ -100,13 +101,13 @@ const SubDiv = styled.div`
   color: #7c8a97;
   margin: 10px 0 23px 0;
   max-width: 80%;
-  font-family: "Roboto", "Helvetica", "Arial", sans-serif;
-`;
+  font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+`
 
 const GridDiv = styled.div`
   display: grid;
   width: 332px;
-`;
+`
 
 const Did = styled.p`
   margin: 0px 0px 12px;
@@ -115,17 +116,25 @@ const Did = styled.p`
   align-items: center;
   color: #7c8a97;
   max-width: 75%;
-  font-family: "Roboto", "Helvetica", "Arial", sans-serif;
-`;
+  font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+`
 
 const ErrorDid = styled.span`
   text-decoration: line-through;
   color: #bbbbbb;
-`;
+`
 
-const SuccessModal = ({ dids, subtitle, text, btnText, closeFn, btnFn }) => {
+const SuccessModal = ({
+  open,
+  dids,
+  subtitle,
+  text,
+  btnText,
+  closeFn,
+  btnFn
+}) => {
   return (
-    <>
+    <Dialog open={open}>
       <Center data-cy="success-modal">
         <Card>
           <CloseIconCont>
@@ -134,22 +143,22 @@ const SuccessModal = ({ dids, subtitle, text, btnText, closeFn, btnFn }) => {
           <CheckIcon />
           <Text>{text}</Text>
           <SubDiv>
-            <Error style={{ color: "red" }} />
+            <Error style={{ color: 'red' }} />
             {subtitle}
           </SubDiv>
           <GridDiv>
-            <P style={{ margin: "13px 0 13px 0" }}>
+            <P style={{ margin: '13px 0 13px 0' }}>
               <strong>
                 {[...dids].filter(did => did.purchased).length} DID's Purchased:
               </strong>
             </P>
             {dids.map(did => (
-              <Did style={{ margin: "0 0 12px 0" }}>
+              <Did style={{ margin: '0 0 12px 0' }}>
                 {did.purchased ? did.number : <ErrorDid>{did.number}</ErrorDid>}
                 {did.purchased ? (
-                  <CheckCircle style={{ color: "#b6d36b", fontSize: 20 }} />
+                  <CheckCircle style={{ color: '#b6d36b', fontSize: 20 }} />
                 ) : (
-                  <Error style={{ color: "red", fontSize: 20 }} />
+                  <Error style={{ color: 'red', fontSize: 20 }} />
                 )}
               </Did>
             ))}
@@ -166,8 +175,8 @@ const SuccessModal = ({ dids, subtitle, text, btnText, closeFn, btnFn }) => {
           </BtnCont>
         </Card>
       </Center>
-    </>
-  );
-};
+    </Dialog>
+  )
+}
 
-export default SuccessModal;
+export default SuccessModal
