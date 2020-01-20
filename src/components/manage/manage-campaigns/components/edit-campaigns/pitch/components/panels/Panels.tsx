701918f8645f98c3027ae5d1c21b7ab6d55fcd2c
Jude Agagad <jude.agagad@boom.camp>
2020-01-20 23:18:42 +0800

    Remove container styles

--- src/components/manage/manage-campaigns/components/edit-campaigns/pitch/components/panels/Panels.tsx
+++ src/components/manage/manage-campaigns/components/edit-campaigns/pitch/components/panels/Panels.tsx
@@ -1,44 +1,45 @@
-import React, { useContext } from 'react';
-import { TableNoResult, SaveButton, TableLoader } from 'common-components';
-import { Add } from '@material-ui/icons';
-import { IdentityContext } from 'contexts/IdentityProvider';
+import React, { useContext } from "react";
+import { TableNoResult, SaveButton, TableLoader } from "common-components";
+import { Add } from "@material-ui/icons";
+import { IdentityContext } from "contexts/IdentityProvider";
 export default function Panels() {
-	const { state } = useContext(IdentityContext);
+  const { state } = useContext(IdentityContext);
 
-	return (
-		<div className="c-default">
-			{state.panels.length > 0 ? (
-				<React.Fragment>{state.loading ? <TableLoader /> : 'Panels'}</React.Fragment>
-			) : (
-				<NoResult />
-			)}
-		</div>
-	);
+  return (
+    <div className="c-default">
+      {state.panels.length > 0 ? (
+        <React.Fragment>
+          {state.loading ? <TableLoader /> : "Panels"}
+        </React.Fragment>
+      ) : (
+        <NoResult />
+      )}
+    </div>
+  );
 }
 
 const NoResult = () => {
-	const { dispatch } = useContext(IdentityContext);
+  const { dispatch } = useContext(IdentityContext);
 
-	return (
-		<TableNoResult
-			headerText="Pitch panels"
-			mainMessage="No pitch panels have been created"
-			subMessage="Would you like to create one? Just hit the “New Panel button."
-			containerStyle={{
-				height: 350,
-				padding: 0
-			}}
-			renderButton={
-				<SaveButton
-					onClick={(e: any) => {
-						e.preventDefault();
-						dispatch({ type: 'CREATE_PANEL', payload: { panel: [ { name: 'xasdas' } ] } });
-					}}
-				>
-					<Add />
-					New Panel
-				</SaveButton>
-			}
-		/>
-	);
+  return (
+    <TableNoResult
+      headerText="Pitch panels"
+      mainMessage="No pitch panels have been created"
+      subMessage="Would you like to create one? Just hit the “New Panel button."
+      renderButton={
+        <SaveButton
+          onClick={(e: any) => {
+            e.preventDefault();
+            dispatch({
+              type: "CREATE_PANEL",
+              payload: { panel: [{ name: "xasdas" }] }
+            });
+          }}
+        >
+          <Add />
+          New Panel
+        </SaveButton>
+      }
+    />
+  );
 };
