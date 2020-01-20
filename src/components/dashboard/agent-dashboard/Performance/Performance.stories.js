d347ec65e9aec1fc259d880254d701e4cb6a248e
Sid Bercasio <sidney.bercasio@boom.camp>
2020-01-20 14:33:54 +0800

    audit and fix vulnerabilities into third party packages

--- src/components/dashboard/agent-dashboard/Performance/Performance.stories.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import Performance from "./Performance";
-import { storiesOf } from "@storybook/react";
-storiesOf("Performance", module).add("default", () => <Performance />);
--- src/components/dashboard/agent-dashboard/agent-table/AgentTable.stories.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import AgentTable from "./AgentTable";
-const stories = storiesOf("Agent Table", module);
-
-stories.add("default", () => <AgentTable />);
