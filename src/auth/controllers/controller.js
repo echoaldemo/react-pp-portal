445ad57ebb906e2201569cc71f3a881648ac465c
Sid Bercasio <sidney.bercasio@boom.camp>
2020-02-05 13:56:00 +0800

    add function to login section

---
+++ src/auth/controllers/controller.js
@@ -0,0 +1,26 @@
+import { post, withToken } from "../../utils/api";
+function login(username, password) {
+  return post("/identity/user/login/", {
+    username,
+    password
+  })
+    .then(res => {
+      localStorage.setItem("ngStorage-ppToken", res.data.auth_token);
+      return true;
+    })
+    .catch(() => false);
+  // const response2 = await withToken(
+  //   `/identity/user/profile/`,
+  //   response.data.auth_token
+  // );
+
+  // localStorage.setItem("ngStorage-ppToken", response.data.auth_token);
+
+  // return true;
+}
+function logout() {
+  localStorage.clear();
+  window.location.href = "/";
+}
+
+export { login, logout };
