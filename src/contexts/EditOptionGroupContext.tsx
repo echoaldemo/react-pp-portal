7961b3ae4d5ce91e15050a71c54619bdfcb50900
Noel Layante <noel.layante@boom.camp>
2020-02-11 17:24:36 +0800

    fix spaces in context

--- src/contexts/EditOptionGroupContext.tsx
+++ src/contexts/EditOptionGroupContext.tsx
@@ -1,62 +1,62 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer } from "react";
 
 const initialState = {
-	loading: true,
-	anchorEl: null,
-	group: {},
-	current: {},
-	edit: {
-		uuid: '',
-		name: '',
-		open: false,
-		edit: false,
-		delete: false,
-		delete2: false,
-		load: false,
-		done: false,
-		done2: false,
-		done3: false,
-		snackErr: '',
-		description: '',
-		value: '',
-		editData: {},
-		error: {
-			description: '',
-			value: ''
-		}
-	},
-	mockData: {
-		uuid: '1b054163-a6a0-4826-b3ec-e304b5aec1c1',
-		slug: 'test-option-group',
-		name: 'test option group',
-		company: 'company 5',
-		options: []
-	}
+  loading: true,
+  anchorEl: null,
+  group: {},
+  current: {},
+  edit: {
+    uuid: "",
+    name: "",
+    open: false,
+    edit: false,
+    delete: false,
+    delete2: false,
+    load: false,
+    done: false,
+    done2: false,
+    done3: false,
+    snackErr: "",
+    description: "",
+    value: "",
+    editData: {},
+    error: {
+      description: "",
+      value: ""
+    }
+  },
+  mockData: {
+    uuid: "1b054163-a6a0-4826-b3ec-e304b5aec1c1",
+    slug: "test-option-group",
+    name: "test option group",
+    company: "company 5",
+    options: []
+  }
 };
 
 const store = createContext<any>(initialState);
 const { Provider } = store;
 
 const StateProvider = ({ children }: any) => {
-	const [ state, dispatch ] = useReducer((state: any, action: any) => {
-		switch (action.type) {
-			case 'LOADING':
-				return { ...state, loading: action.payload.loading };
-			case 'GROUP':
-				return { ...state, group: action.payload.group };
-			case 'CURRENT':
-				return { ...state, current: action.payload.current };
-			case 'EDIT':
-				return { ...state, edit: action.payload.edit };
-			case 'ANCHOR_EL':
-				return { ...state, anchorEl: action.payload.anchorEl };
-			case 'MOCK':
-				return { ...state, mockData: action.payload.mockData };
-			default:
-				return null;
-		}
-	}, initialState);
+  const [state, dispatch] = useReducer((state: any, action: any) => {
+    switch (action.type) {
+      case "LOADING":
+        return { ...state, loading: action.payload.loading };
+      case "GROUP":
+        return { ...state, group: action.payload.group };
+      case "CURRENT":
+        return { ...state, current: action.payload.current };
+      case "EDIT":
+        return { ...state, edit: action.payload.edit };
+      case "ANCHOR_EL":
+        return { ...state, anchorEl: action.payload.anchorEl };
+      case "MOCK":
+        return { ...state, mockData: action.payload.mockData };
+      default:
+        return null;
+    }
+  }, initialState);
 
-	return <Provider value={{ state, dispatch }}>{children}</Provider>;
+  return <Provider value={{ state, dispatch }}>{children}</Provider>;
 };
 export { store, StateProvider };
--- src/contexts/OptionGroupContext.tsx
+++ src/contexts/OptionGroupContext.tsx
@@ -1,87 +1,87 @@
-import React, { createContext, useReducer } from 'react'
+import React, { createContext, useReducer } from "react";
 
 const initialState = {
   groupList: [],
   paginateList: [],
   groupState: {
-    name: '',
+    name: "",
     load: false,
     done: false,
     load2: false,
     done2: false,
     create: false,
     delete: false,
-    errMsg: ''
+    errMsg: ""
   },
   mockData: [
     {
-      uuid: '1b6eba8a-89de-4105-b6f8-1d248ed3483c',
-      slug: 'slug 4',
-      name: 'name 4',
-      company: 'company 4',
+      uuid: "1b6eba8a-89de-4105-b6f8-1d248ed3483c",
+      slug: "slug 4",
+      name: "name 4",
+      company: "company 4",
       options: [
         {
-          uuid: 'd75f6370-ea08-445a-afd6-080762632e4b',
-          description: 'test',
-          value: '1'
+          uuid: "d75f6370-ea08-445a-afd6-080762632e4b",
+          description: "test",
+          value: "1"
         },
         {
-          uuid: 'a688c742-7cdc-4df3-9bd6-3c6e5c259f7a',
-          description: 'wqer',
-          value: '1'
+          uuid: "a688c742-7cdc-4df3-9bd6-3c6e5c259f7a",
+          description: "wqer",
+          value: "1"
         }
       ]
     },
     {
-      uuid: '1b054163-a6a0-4826-b3ec-e304b5aec1c1',
-      slug: 'slug 5',
-      name: 'name 5',
-      company: 'company 5',
+      uuid: "1b054163-a6a0-4826-b3ec-e304b5aec1c1",
+      slug: "slug 5",
+      name: "name 5",
+      company: "company 5",
       options: []
     },
     {
-      uuid: 'a33b05a3-0968-40d0-8d0f-ed5debee0a62',
-      slug: 'slug 6',
-      name: 'name 6',
-      company: 'company 6',
+      uuid: "a33b05a3-0968-40d0-8d0f-ed5debee0a62",
+      slug: "slug 6",
+      name: "name 6",
+      company: "company 6",
       options: []
     },
     {
-      uuid: '75e95f1c-0015-4f95-aa61-a530ee407584',
-      slug: 'slug 7',
-      name: 'name 7',
-      company: 'company 7',
+      uuid: "75e95f1c-0015-4f95-aa61-a530ee407584",
+      slug: "slug 7",
+      name: "name 7",
+      company: "company 7",
       options: []
     },
     {
-      uuid: '58240cbd-17c0-40b4-848f-267ad2bd6222',
-      slug: 'slug 9',
-      name: 'name 9',
-      company: 'company 9',
+      uuid: "58240cbd-17c0-40b4-848f-267ad2bd6222",
+      slug: "slug 9",
+      name: "name 9",
+      company: "company 9",
       options: []
     }
   ]
-}
+};
 
-const store = createContext<any>(initialState)
-const { Provider } = store
+const store = createContext<any>(initialState);
+const { Provider } = store;
 
 const StateProvider = ({ children }: any) => {
   const [state, dispatch] = useReducer((state: any, action: any) => {
     switch (action.type) {
-      case 'GROUP_LIST':
-        return { ...state, groupList: action.payload.groupList }
-      case 'PAGINATE':
-        return { ...state, paginateList: action.payload.paginateList }
-      case 'GROUP_STATE':
-        return { ...state, groupState: action.payload.groupState }
-      case 'MOCK':
-        return { ...state, mockData: action.payload.mockData }
+      case "GROUP_LIST":
+        return { ...state, groupList: action.payload.groupList };
+      case "PAGINATE":
+        return { ...state, paginateList: action.payload.paginateList };
+      case "GROUP_STATE":
+        return { ...state, groupState: action.payload.groupState };
+      case "MOCK":
+        return { ...state, mockData: action.payload.mockData };
       default:
-        return null
+        return null;
     }
-  }, initialState)
+  }, initialState);
 
-  return <Provider value={{ state, dispatch }}>{children}</Provider>
-}
-export { store, StateProvider }
+  return <Provider value={{ state, dispatch }}>{children}</Provider>;
+};
+export { store, StateProvider };
