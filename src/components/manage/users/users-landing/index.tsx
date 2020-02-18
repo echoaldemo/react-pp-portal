1a8389502a38ad8b007c123b3e18ddee6dd0d529
Sid Bercasio <sidney.bercasio@boom.camp>
2020-02-18 09:45:14 +0800

    fix infinite loop in users section

--- src/components/manage/users/users-landing/index.tsx
+++ src/components/manage/users/users-landing/index.tsx
@@ -32,39 +32,10 @@ const UserLanding = () => {
   // *** FETCHING DATA USING API UTIL
   useEffect(() => {
     setLoading(true);
-
-    Promise.all([
-      get("/identity/user/manage/list/", {
-        limit: 9999,
-        order_by: "-datetime_modified"
-      }),
-      get("/identity/company/list/"),
-      get("/identity/campaign/list/"),
-      get("/identity/group/list/"),
-      get("/identity/team/list/"),
-      get("/identity/realm/list/")
-    ]).then(function(values) {
+    if (state.roles.length !== 0) {
       setLoading(false);
-      const userList = values[0].data.results;
-      const companyList = values[1].data;
-      const campaignList = values[2].data;
-      const roleList = values[3].data;
-      const teamList = values[4].data;
-      const realmList = values[5].data;
-
-      dispatch({
-        type: "manage-list",
-        payload: {
-          userList,
-          companyList,
-          campaignList,
-          roleList,
-          teamList,
-          realmList
-        }
-      });
-    });
-  }, [dispatch]);
+    }
+  }, []);
 
   const paginate: Function = (from: any, to: any) => {
     setUserData(users.slice(from, to));
