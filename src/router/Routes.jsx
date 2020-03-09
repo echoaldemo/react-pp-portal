fb45e85b1cc6d6f320ac6cc786a78d48cf832f14
Sid Bercasio <sidney.bercasio@boom.camp>
2020-03-09 11:11:16 +0800

    add routes of station

--- src/router/Routes.jsx
+++ src/router/Routes.jsx
@@ -54,6 +54,7 @@ import ChangePassword from "auth/change-password/ChangePassword";
 
 //API UTIL
 import { get } from "utils/api";
+import Stations from "components/stations/Stations";
 
 export default function Routes() {
   const [person, setPerson] = useState([]);
@@ -164,6 +165,7 @@ export default function Routes() {
             path="/manage/locations/edit/:uuid/"
             component={LocationSettings}
           />
+          <PrivateRoute path="/manage/stations" component={Stations} />
           {/* Global Pitch routes */}
           <PrivateRoute
             path="/manage/global-pitch-phrasebooks"
