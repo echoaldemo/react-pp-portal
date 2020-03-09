2c9f107b5a15fa79604d5e8f0dfda854b33105d8
Andrea Hernandez Monterrubio <andrea@boomsourcing.com>
2020-03-09 09:55:11 -0600

    Adding Michelle's changes

--- src/components/manage/manage-campaigns/components/edit-campaigns/settings/content/changelog/components/change-log-table.tsx
+++ src/components/manage/manage-campaigns/components/edit-campaigns/settings/content/changelog/components/change-log-table.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
-
+import React, {useState} from 'react';
 import {AsyncTable, TableNoResult} from 'common-components'
 import {TableRow, TableCell} from '@material-ui/core'
-
+import styled from 'styled-components'
+import {
+  KeyboardArrowDown,
+  KeyboardArrowUp
+} from '@material-ui/icons'
 const classes = {
     linkStyle: {
         color: '#333',
@@ -10,13 +13,30 @@ const classes = {
         cursor: 'pointer'
     }
 }
-
+const Span = styled.span`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`
 interface Props {
     tableData : Array<Object>,
     setActiveData: (change: any) => void
+    onSortData: (isSorted: boolean, type:string) => void
 }
-
-const ChangeLogTable : React.FC < Props > = ({tableData, setActiveData}) => {
+const ChangeLogTable : React.FC < Props > = ({tableData, setActiveData, onSortData}) => {
+    const [sortedUser, setSortedUser] = useState(true)
+    const [sortedCreated, setSortedCreated] = useState(true)
+  
+    const onSort = (type:string) => {
+      if (type === 'user') {
+        setSortedUser(!sortedUser)
+        onSortData(!sortedUser, type)
+      }
+      if (type === 'created') {
+        setSortedCreated(!sortedCreated)
+        onSortData(!sortedCreated, type)
+      }
+    }
     const NoResult = () => {
         return (
           <TableNoResult
@@ -27,12 +47,19 @@ const ChangeLogTable : React.FC < Props > = ({tableData, setActiveData}) => {
           />
         )
     }
-
     return (
         <React.Fragment>
             {tableData.length > 0
                 ? (<AsyncTable
-                    headers={['Users', 'Created', 'Time', 'Changes in', '']}
+                    headers={[
+                    <Span onClick={() => onSort('user')}>
+                      User
+                      {sortedUser ? <KeyboardArrowDown /> : <KeyboardArrowUp />}
+                    </Span>, 
+                    <Span onClick={() => onSort('created')}>
+                      Created
+                      {sortedCreated ? <KeyboardArrowDown /> : <KeyboardArrowUp />}
+                    </Span>, 'Time', 'Changes in', '']}
                     tableData={tableData}
                     render={(changes : any, {row, cell, icon} : any) => {
                     return changes.map((change : any, i : number) => (
@@ -61,5 +88,4 @@ const ChangeLogTable : React.FC < Props > = ({tableData, setActiveData}) => {
         </React.Fragment>
     )
 }
-
 export default ChangeLogTable
--- src/components/manage/manage-campaigns/components/edit-campaigns/settings/content/changelog/index.tsx
+++ src/components/manage/manage-campaigns/components/edit-campaigns/settings/content/changelog/index.tsx
@@ -1,76 +1,71 @@
 import React, {useContext, useState, useEffect} from 'react';
 import {IdentityContext} from "contexts/IdentityProvider"
 import {post} from "utils/api"
-
 import LogsFilter from './logs-filter';
 import { Modal, TableLoader } from 'common-components';
 import ChangeLogTable from './components/change-log-table'
 import ModalDetails from './components/modal-details';
-
 const ChangeLog: React.FC= () => {
-	const [data, setData] = useState([]);
-	const [origData, setOrigData] = useState([]);
-	const [activeData, setActiveData] = useState({});
-	const [openModal, setOpenModal] = useState(false);
-	const { state } = useContext(IdentityContext);
-	const {campaignDetails } = state
-
-	const handleFilterUpdate = (data: any) => {
-		setData(data);
-	};
-
-	const handleActiveData = (data: any) => {
-		setActiveData(data);
-		setOpenModal(true);
-	};
-
-	const handleCloseModal = () => {
-		setOpenModal(false);
-	};
-
-	const getData = () => {
-		post(`/identity/changelog/filter/`, {
-			campaign: campaignDetails.slug,
-		  } )
-		.then((res:any) => {
-			setData(res.data.data)
-			setOrigData(res.data.data)
-		}).catch((err:any) => {
-			console.error(err); 
-		  })
-	}
-
-	useEffect(() => {
-		if("uuid" in campaignDetails){ 
-			getData();
-		}
-	}, [campaignDetails])
-
-	return (
-		<div>
-			{state.loading ? 
-				<TableLoader /> :
-				<React.Fragment> 
-					{ data.length > 0 && 
-						<LogsFilter
-						data={data}
-						originalData={origData}
-						handleFilterUpdate={handleFilterUpdate}
-						modalFunc={handleActiveData} /> 
-					}
-
-					<ChangeLogTable tableData={data} setActiveData={handleActiveData} />
-
-				{ activeData && (
-					<Modal open={openModal} title="Change Details" onClose={handleCloseModal} width={651}>
-						<ModalDetails data={activeData} onClose={handleCloseModal} />
-					</Modal>) 
-				}
-				</React.Fragment>
-		}
-			
-		</div>
-	);
+    const [data, setData] = useState([]);
+    const [origData, setOrigData] = useState([]);
+    const [activeData, setActiveData] = useState({});
+    const [openModal, setOpenModal] = useState(false);
+    const { state } = useContext(IdentityContext);
+    const {campaignDetails } = state
+    const handleFilterUpdate = (data: any) => {
+        setData(data);
+    };
+    const handleActiveData = (data: any) => {
+        setActiveData(data);
+        setOpenModal(true);
+    };
+    const handleCloseModal = () => {
+        setOpenModal(false);
+    };
+    const getData = () => {
+        post(`/identity/changelog/filter/`, {
+            campaign: campaignDetails.slug,
+          } )
+        .then((res:any) => {
+            setData(res.data.data)
+            setOrigData(res.data.data)
+        }).catch((err:any) => {
+            console.error(err); 
+          })
+    }
+    const onSortData = (isSorted:boolean, type:string) => {
+        let results = []
+        if (isSorted)
+            results = data.sort((a, b) => (a[type] > b[type] ? -1 : 1))
+        else results = data.sort((a, b) => (a[type] < b[type] ? -1 : 1))
+        setData(results)
+    }
+    useEffect(() => {
+        if("uuid" in campaignDetails){ 
+            getData();
+        }
+    }, [campaignDetails])
+    return (
+        <div>
+            {state.loading ? 
+                <TableLoader /> :
+                <React.Fragment> 
+                    { data.length > 0 && 
+                        <LogsFilter
+                        data={data}
+                        originalData={origData}
+                        handleFilterUpdate={handleFilterUpdate}
+                        modalFunc={handleActiveData} /> 
+                    }
+                    <ChangeLogTable onSortData={(isSorted:boolean, type:string) => onSortData(isSorted, type)} tableData={data} setActiveData={handleActiveData} />
+                { activeData && (
+                    <Modal open={openModal} title="Change Details" onClose={handleCloseModal} width={651}>
+                        <ModalDetails data={activeData} onClose={handleCloseModal} />
+                    </Modal>) 
+                }
+                </React.Fragment>
+            }
+        </div>
+    );
 };
-
 export default ChangeLog;
