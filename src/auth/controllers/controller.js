9f176a97b7248860fb86e02106d99d491110f6b1
Ulises Castro <ulisescastro2015@gmail.com>
2020-03-11 11:35:51 -0600

    Removed commented lines

--- src/auth/controllers/controller.js
+++ src/auth/controllers/controller.js
@@ -1,13 +1,19 @@
-import { post, withToken } from "../../utils/api";
+import {
+  post,
+  withToken
+} from "../../utils/api";
+
 function login(username, password) {
   return post("/identity/user/login/", {
-    username,
-    password
-  })
+      username,
+      password
+    })
     .then(res => {
       localStorage.setItem("ngStorage-ppToken", res.data.auth_token);
       return withToken(`/identity/user/profile/`, res.data.auth_token)
-        .then(({data}) => {
+        .then(({
+          data
+        }) => {
           localStorage.setItem("user", data.first_name);
           localStorage.setItem("uuid", data.uuid);
           localStorage.setItem("type", data.groups[0]);
@@ -16,18 +22,14 @@ function login(username, password) {
         .catch(err => console.log(err));
     })
     .catch(() => false);
-  // const response2 = await withToken(
-  //   `/identity/user/profile/`,
-  //   response.data.auth_token
-  // );
-
-  // localStorage.setItem("ngStorage-ppToken", response.data.auth_token);
-
-  // return true;
 }
+
 function logout() {
   localStorage.clear();
   window.location.href = "/";
 }
 
-export { login, logout };
+export {
+  login,
+  logout
+};
